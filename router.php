cece60d5d2a12270ec9c2c1cc6a18188f8ce2b6a
John Fellman <john.fellman@avrs.com>
2015-03-09 15:21:03 -0700

    adds put

--- router.php
+++ router.php
@@ -119,7 +119,7 @@ class Request {
         case 'PUT':
                 $model = $this->search($this->data, 'id', $this->id);
                 $model = array_merge($model, $this->rq_params);
-                $this->result = json_encode($this->data);
+                $this->result = json_encode($model);
             break;
         case 'DELETE':
             break;
