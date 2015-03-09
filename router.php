09ea4d7ba73d1ed5958da26869bc3d982d6ab34d
John Fellman <john.fellman@avrs.com>
2015-03-09 12:16:07 -0700

    debugging

--- router.php
+++ router.php
@@ -31,20 +31,10 @@ if ($id) {
 
 echo $result;
 
-function search($array, $key, $value)
-{
-    $results = array();
-
-    if (is_array($array)) {
-        if (isset($array[$key]) && $array[$key] == $value) {
-            $results[] = $array;
-        }
-
-        foreach ($array as $subarray) {
-            $results = array_merge($results, search($subarray, $key, $value));
-        }
-    }
-
-    return $results;
+public function search(Array $array, $key, $value) {   
+    foreach ($array as $subarray){  
+        if (isset($subarray[$key]) && $subarray[$key] == $value)
+            return $subarray;       
+    } 
 }
 ?>
