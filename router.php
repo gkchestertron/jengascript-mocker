c809a61a9d9a8a0cba50bad3f8e79d4b3edc45f1
John Fellman <john.fellman@avrs.com>
2015-03-09 11:57:48 -0700

    adds ability to get first item in list

--- router.php
+++ router.php
@@ -4,17 +4,27 @@ header('Access-Control-Allow-Methods: GET, POST');
 $requestURI = explode('/', $_SERVER['REQUEST_URI']);
 $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
 
+// handles case where you are not in home directory
 for ($i= 0; $i < sizeof($scriptName); $i++) {
     if ($requestURI[$i] == $scriptName[$i]) {
         unset($requestURI[$i]);
     }
 }
 
-$path = array_values($requestURI);
-unset($path[sizeof($path) - 1]);
+$path = array_values($requestURI); // path as an array
+$id = array_pop($path); // get id or lack thereof
 $path[sizeof($path) - 1] = $path[sizeof($path) - 1] . '.json';
 
 $file = fopen(implode($path, '/'), 'r') or http_response_code(404);
-echo fread($file, filesize(implode($path, '/')));
+$file_data = fread($file, filesize(implode($path, '/')));
 fclose($file);
+
+if ($id) {
+    $data = json_decode($file_data);
+    $result = json_encode($data[0]);
+} else {
+    $result = $file_data;
+}
+
+echo $result;
 ?>
