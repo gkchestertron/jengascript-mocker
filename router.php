5b61b6ce3b428ab21199f5181a3786e99e75f350
John Fellman <john.fellman@avrs.com>
2015-03-09 13:40:13 -0700

    makes it classy

--- router.php
+++ router.php
@@ -1,72 +1,85 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-$requestURI = explode('/', $_SERVER['REQUEST_URI']);
-$scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
-$result = null;
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
-// handles case where you are not in home directory
-for ($i= 0; $i < sizeof($scriptName); $i++) {
-    if ($requestURI[$i] == $scriptName[$i]) {
-        unset($requestURI[$i]);
+$request = new Request();
+class Request {
+    public $rq_uri;
+    public $rq_method;
+    public $result = null;
+    public $data;
+    public $file_data;
+
+    public function __construct() {
+        $this->rq_uri    = explode('/', $_SERVER['REQUEST_URI'];
+        $this->rq_method = $_SERVER['REQUEST_METHOD'];
+        $this->getData();
+        $this->processInput();
+        $this->respond();
     }
-}
 
-$path = array_values($requestURI); // path as an array
-$id = array_pop($path); // get id or lack thereof
-$path[sizeof($path) - 1] = $path[sizeof($path) - 1] . '.json';
+    public function getData() {
+        $path = array_values($this->rq_uri); // path as an array
+        $id = array_pop($path); // get id or lack thereof
+        $path[sizeof($path) - 1] = $path[sizeof($path) - 1] . '.json';
 
-$file_path = implode($path, '/');
+        $file_path = implode($path, '/');
 
-if (file_exists($file_path . '.temp')) {
-    $file = fopen($file_path. '.temp', 'r') or http_response_code(500);
-} else if (file_exists($file_path)) {
-    $old_file = fopen($file_path, 'r') or http_response_code(500);
-    $new_file = fopen($file_path . '.temp', 'w');
-    
-    $file_data = fread($old_file, filesize($file_path));
-    fwrite($new_file, $file_data);
-    fclose($old_file);
-    fclose($new_file);
-    $file = fopen($file_path. '.temp', 'r') or http_response_code(500);
-} else {
-    http_response_code(404);
-}
- 
-$file_data = fread($file, filesize($file_path));
-fclose($file);
-
-switch ($_SERVER['REQUEST_METHOD']) {
-case 'GET':
-    if ($id) {
-        $data   = json_decode($file_data, true);
-        $model  = search($data, 'id', $id);
-
-        if ($model) {
-            $result = json_encode($model);
+        if (file_exists($file_path . '.temp')) {
+            $file = fopen($file_path. '.temp', 'r') or http_response_code(500);
+        } else if (file_exists($file_path)) {
+            $old_file = fopen($file_path, 'r') or http_response_code(500);
+            $new_file = fopen($file_path . '.temp', 'w');
+            
+            $this->file_data = fread($old_file, filesize($file_path));
+            fwrite($new_file, $this->file_data);
+            fclose($old_file);
+            fclose($new_file);
+            $file = fopen($file_path. '.temp', 'r') or http_response_code(500);
+        } else {
+            http_response_code(404);
         }
-    } else {
-        $result = $file_data;
+         
+        $this->file_data = fread($file, filesize($file_path));
+        fclose($file);
+        $this->data = json_decode($this->file_data, true);
     }
-    break;
-case 'POST':
-    break;
-case 'PUT':
-    break;
-case 'DELETE':
-    break;
-}
+    
+    public function processInput() {
+        switch ($this->rq_method) {
+        case 'GET':
+            if ($id) {
+                $model  = $this->search($this->data, 'id', $id);
 
-if (isset($result)) {
-    echo $result;
-} else {
-    http_response_code(404);
-}
+                if ($model) {
+                    $this->result = json_encode($model);
+                }
+            } else {
+                $this->result = $this->file_data;
+            }
+            break;
+        case 'POST':
+            break;
+        case 'PUT':
+            break;
+        case 'DELETE':
+            break;
+        }
+    }
 
-function search($array, $key, $value) {   
-    foreach ($array as $subarray){  
-        if (isset($subarray[$key]) && $subarray[$key] == $value)
-            return $subarray;       
-    } 
+    public function respond() {
+        if (isset($this->result)) {
+            echo $this->result;
+        } else {
+            http_response_code(404);
+        }
+    }
+
+    public function search($array, $key, $value) {   
+        foreach ($array as $subarray){  
+            if (isset($subarray[$key]) && $subarray[$key] == $value)
+                return $subarray;       
+        } 
+    }
 }
 ?>
