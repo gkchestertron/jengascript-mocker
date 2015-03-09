ba183343b56222f606d6969f268e792c5018b968
John Fellman <john.fellman@avrs.com>
2015-03-09 15:31:45 -0700

    adds put

--- router.php
+++ router.php
@@ -125,6 +125,7 @@ class Request {
                 $model[$key] = $value;
             }
 
+            array_push($this->data, $model);
             $result = json_encode($this->data);
             $file   = fopen($this->file_path, 'w');
             fwrite($file, $result);
