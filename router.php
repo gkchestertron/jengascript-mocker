0d8683d6ca7e0d3b82f175700cac3a42818b2dca
John Fellman <john.fellman@avrs.com>
2015-03-09 14:00:15 -0700

    makes it classy

--- router.php
+++ router.php
@@ -5,6 +5,7 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 $request = new Request();
 
 echo file_exists('/data.json');
+echo 'some shit';
 
 class Request {
     public $rq_uri;
